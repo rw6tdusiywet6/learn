438448d623aa6c7f127c6dc507bc2b812730e9fa
rw6tdusiywet6 <ssjcdfsdfsdgsdf@hotmail.com>
2024-05-04 01:18:15 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const chunk = (arr, size) =>
-  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
-    arr.slice(i * size, i * size + size),
-  );
+function coinChange(coins, amount) {
+  const dp = new Array(amount + 1).fill(amount + 1);
+  dp[0] = 0;
+  for (const coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+    }
+  }
+  return dp[amount] > amount ? -1 : dp[amount];
+}
